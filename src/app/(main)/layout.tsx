6b3a388a8252116eadb6b88160fa147482415688
augesrob <rfa1991@gmail.com>
2026-03-15 23:28:21 -0500

    Attempting clean path aliases for Navbar and Footer

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,5 +1,5 @@
-import Navbar from "../../components/Navbar"; // Going up two levels to reach src/components
-import Footer from "../../components/Footer";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 export default function MainLayout({
   children,
